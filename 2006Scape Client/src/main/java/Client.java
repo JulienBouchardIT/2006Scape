7afe013464b5ed8992da696f00479f127623f474
Julien <45082899+JulienBouchardIT@users.noreply.github.com>
2022-04-30 00:33:02 -0400

    Update Client.java

--- 2006Scape Client/src/main/java/Client.java
+++ 2006Scape Client/src/main/java/Client.java
@@ -1,6 +1,6 @@
 public final class Client {
 	public static void main(String[] args) {
-		ClientSettings.SERVER_IP = "localhost";
+		ClientSettings.SERVER_IP = "192.168.2.100";
 		Main.main(args);
 	}
 }
